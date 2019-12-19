a4bf59eed7cf74a4726f42e157025da5ad7978c9
Arthur <ath.engels@gmail.com>
2019-12-19 14:38:45 +0100

    intenavbar

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -9,6 +9,11 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Faker;
 class ArticleFixtures extends Fixture implements DependentFixtureInterface
 {
+    const Article =[
+        'images/articles/articles1.jpeg',
+        'images/articles/articles2.jpeg'
+
+    ];
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
@@ -18,7 +23,7 @@ class ArticleFixtures extends Fixture implements DependentFixtureInterface
             $article->setCategory($this->getReference('categorie_'.random_int(1,4)));
             $article->setName($faker->domainName);
             $article->setDescription($faker->text);
-            $article->setPicture($faker->imageUrl());
+            $article->setPicture(array_rand(self::Article));
             $manager->persist($article);
         }
 
